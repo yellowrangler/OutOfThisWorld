ffee279ed087330aa936ba6a1b879f6010d7e610
yellowrangler <tarrant.cutler@gmail.com>
2014-02-11 11:04:02 -0500

    Initial Commit

---
+++ Out Of This World/OWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OWAppDelegate.h
+//  Out Of This World
+//
+//  Created by Tarrant Cutler on 2/11/14.
+//  Copyright (c) 2014 Tarrant Cutler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out Of This World/OWViewController.m
@@ -0,0 +1,29 @@
+//
+//  OWViewController.m
+//  Out Of This World
+//
+//  Created by Tarrant Cutler on 2/11/14.
+//  Copyright (c) 2014 Tarrant Cutler. All rights reserved.
+//
+
+#import "OWViewController.h"
+
+@interface OWViewController ()
+
+@end
+
+@implementation OWViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
