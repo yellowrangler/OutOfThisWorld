d651269d97cd2cab0c04e1f31bf32f9e028ac688
yellowrangler <tarrant.cutler@gmail.com>
2014-02-11 18:47:32 -0500

    Out of This World
    Added two files

--- Out Of This World/OWSpaceObject.h
+++ Out Of This World/OWSpaceObject.h
@@ -10,4 +10,18 @@
 
 @interface OWSpaceObject : NSObject
 
+@property (strong, nonatomic) NSString *name;
+@property (nonatomic) float gravitationalForce;
+@property (nonatomic) float diameter;
+@property (nonatomic) float yearLength;
+@property (nonatomic) float dayLength;
+@property (nonatomic) float temperature;
+@property (nonatomic) int numberOfMoons;
+@property (strong, nonatomic) NSString *nickname;
+@property (strong, nonatomic) NSString *interestingFact;
+
+@property (strong, nonatomic) UIImage *spaceImage;
+
+-(id)initWithData:(NSDictionary *)data andImage:(UIImage *)image;
+
 @end
